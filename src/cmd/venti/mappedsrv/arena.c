ed73073f7a867e5c228117dff0afa1d38aa8a1cf
WB Kloke <wb@pkeus.de>
2026-01-03 15:08:02 +0100

    neue Datei:     arena.c	mapped version of ../srv 	neue Datei:     arenas.c	mapped version of ../srv 	neue Datei:     clump.c	mapped version of ../srv 	neue Datei:     config.c	mapped version of ../srv 	neue Datei:     conv.c	mapped version of ../srv 	neue Datei:     dat.h	mapped version of ../srv 	neue Datei:     fns.h	mapped version of ../srv 	neue Datei:     graph.c	mapped version of ../srv 	neue Datei:     hdisk.c	mapped version of ../srv 	neue Datei:     hproc.c	mapped version of ../srv 	neue Datei:     httpd.c	mapped version of ../srv 	neue Datei:     ifile.c	mapped version of ../srv 	neue Datei:     lump.c	mapped version of ../srv 	neue Datei:     lumpcache.c	mapped version of ../srv 	neue Datei:     mkfile	mapped version of ../srv 	neue Datei:     mventi.c	new 	neue Datei:     part.c	mapped version of ../srv 	neue Datei:     png.c	mapped version of ../srv 	neue Datei:     score.c	mapped version of ../srv 	neue Datei:     stats.c	mapped version of ../srv 	neue Datei:     stdinc.h	mapped version of ../srv 	neue Datei:     syncarena.c	mapped version of ../srv 	neue Datei:     trace.c	mapped version of ../srv 	neue Datei:     trie.c	mapped version of ../srv 	neue Datei:     unittoull.c	mapped version of ../srv 	neue Datei:     unwhack.c	mapped version of ../srv 	neue Datei:     utils.c	mapped version of ../srv 	neue Datei:     whack.c	mapped version of ../srv 	neue Datei:     whack.h	mapped version of ../srv 	neue Datei:     xml.c	mapped version of ../srv 	neue Datei:     xml.h	mapped version of ../srv 	neue Datei:     zblock.c	mapped version of ../srv 	gelöscht:       ../srv/buildtrie.c unused

---
+++ src/cmd/venti/mappedsrv/arena.c
@@ -0,0 +1,566 @@
+#include "stdinc.h"
+#include "dat.h"
+#include "fns.h"
+
+/* this version of arena only works when the arena partition is mmap'ed */
+typedef struct ASum ASum;
+
+struct ASum
+{
+	Arena	*arena;
+	ASum	*next;
+};
+
+static void	sealarena(Arena *arena);
+static int	okarena(Arena *arena);
+static int	loadarena(Arena *arena);
+static CIBlock	*getcib(Arena *arena, int clump, int writing, CIBlock *rock);
+static void	putcib(Arena *arena, CIBlock *cib);
+static void	sumproc(void *);
+
+static QLock	sumlock;
+static Rendez	sumwait;
+static ASum	*sumq;
+static ASum	*sumqtail;
+
+int	arenasumsleeptime;
+
+int
+initarenasum(void)
+{
+	needzeroscore();  /* OS X */
+
+	qlock(&sumlock);
+	sumwait.l = &sumlock;
+	qunlock(&sumlock);
+
+	if(vtproc(sumproc, nil) < 0){
+		seterr(EOk, "can't start arena checksum slave: %r");
+		return -1;
+	}
+	return 0;
+}
+
+/*
+ * make an Arena, and initialize it based upon the disk header and trailer.
+ */
+Arena*
+initarena(Part *part, u64int base, u64int size, u32int blocksize)
+{
+	Arena *arena;
+
+	arena = MKZ(Arena);
+	arena->part = part;
+	arena->blocksize = blocksize;
+	arena->clumpmax = arena->blocksize / ClumpInfoSize;
+	arena->base = base + blocksize;
+	arena->size = size - 2 * blocksize;
+
+	if(loadarena(arena) < 0){
+		seterr(ECorrupt, "arena header or trailer corrupted");
+		freearena(arena);
+		return nil;
+	}
+	if(okarena(arena) < 0){
+		freearena(arena);
+		return nil;
+	}
+
+	if(arena->diskstats.sealed && scorecmp(zeroscore, arena->score)==0)
+		sealarena(arena);
+
+	return arena;
+}
+
+void
+freearena(Arena *arena)
+{
+	if(arena == nil)
+		return;
+	free(arena);
+}
+
+int
+readclumpinfo(Arena *arena, int clump, ClumpInfo *ci)
+{
+	CIBlock *cib, r;
+
+	cib = getcib(arena, clump, 0, &r);
+	if(cib == nil)
+		return -1;
+	unpackclumpinfo(ci, &cib->data->data[cib->offset]);
+	putcib(arena, cib);
+	return 0;
+}
+
+int
+readclumpinfos(Arena *arena, int clump, ClumpInfo *cis, int n)
+{
+	CIBlock *cib, r;
+	int i;
+
+	/*
+	 * because the clump blocks are laid out
+	 * in reverse order at the end of the arena,
+	 * it can be a few percent faster to read
+	 * the clumps backwards, which reads the
+	 * disk blocks forwards.
+	 */
+	for(i = n-1; i >= 0; i--){
+		cib = getcib(arena, clump + i, 0, &r);
+		if(cib == nil){
+			n = i;
+			continue;
+		}
+		unpackclumpinfo(&cis[i], &cib->data->data[cib->offset]);
+		putcib(arena, cib);
+	}
+	return n;
+}
+
+/*
+ * write directory information for one clump
+ * must be called the arena locked
+ */
+int
+writeclumpinfo(Arena *arena, int clump, ClumpInfo *ci)
+{
+	CIBlock *cib, r;
+
+	cib = getcib(arena, clump, 1, &r);
+	if(cib == nil)
+		return -1;
+//	dirtydblock(cib->data, DirtyArenaCib);
+	packclumpinfo(ci, &cib->data->data[cib->offset]);
+	putcib(arena, cib);
+	return 0;
+}
+
+u64int
+arenadirsize(Arena *arena, u32int clumps)
+{
+	return ((clumps / arena->clumpmax) + 1) * arena->blocksize;
+}
+
+/*
+ * read a clump of data
+ * n is a hint of the size of the data, not including the header
+ * make sure it won't run off the end, then return the number of bytes actually read
+ */
+u32int
+readarena(Arena *arena, u64int aa, u8int *buf, long n)
+{
+	u64int a;
+
+	if(n == 0)
+		return -1;
+
+	qlock(&arena->lock);
+	a = arena->size - arenadirsize(arena, arena->memstats.clumps);
+	qunlock(&arena->lock);
+	if(aa >= a){
+		seterr(EOk, "reading beyond arena clump storage: clumps=%d aa=%lld a=%lld -1 clumps=%lld\n", arena->memstats.clumps, aa, a, arena->size - arenadirsize(arena, arena->memstats.clumps - 1));
+		return -1;
+	}
+	if(aa + n > a)
+		n = a - aa;
+	memmove(buf, arena->part->mapped+arena->base+aa,n);
+	return n;
+}
+
+/*
+ * write some data to the clump section at a given offset
+ * used to fix up corrupted arenas.
+ */
+u32int
+writearena(Arena *arena, u64int aa, u8int *clbuf, u32int n)
+{
+	u64int a;
+
+	if(n == 0)
+		return -1;
+
+	qlock(&arena->lock);
+	a = arena->size - arenadirsize(arena, arena->memstats.clumps);
+	if(aa >= a || aa + n > a){
+		qunlock(&arena->lock);
+		seterr(EOk, "writing beyond arena clump storage");
+		return -1;
+	}
+	memmove(arena->part->mapped+aa,clbuf, n);
+
+	qunlock(&arena->lock);
+	return n;
+}
+
+/*
+ * allocate space for the clump and write it,
+ * updating the arena directory
+ZZZ question: should this distinguish between an arena
+filling up and real errors writing the clump?
+ */
+u64int
+writeaclump(Arena *arena, Clump *c, u8int *clbuf)
+{
+	u32int n;
+	u64int aa;
+
+	n = c->info.size + ClumpSize + U32Size;
+	qlock(&arena->lock);
+	aa = arena->memstats.used;
+	if(arena->memstats.sealed
+	|| aa + n + U32Size + arenadirsize(arena, arena->memstats.clumps + 1) > arena->size){
+		if(!arena->memstats.sealed){
+			logerr(EOk, "seal memstats %s", arena->name);
+			arena->memstats.sealed = 1;
+			wbarena(arena);
+		}
+		qunlock(&arena->lock);
+		return TWID64;
+	}
+	if(packclump(c, &clbuf[0], arena->clumpmagic) < 0){
+		qunlock(&arena->lock);
+		return TWID64;
+	}
+	memmove(clbuf, arena->part->mapped+aa,n);
+	arena->memstats.used += c->info.size + ClumpSize;
+	arena->memstats.uncsize += c->info.uncsize;
+	if(c->info.size < c->info.uncsize)
+		arena->memstats.cclumps++;
+	arena->memstats.clumps++;
+	if(arena->memstats.clumps == 0)
+		sysfatal("clumps wrapped");
+	arena->wtime = now();
+	if(arena->ctime == 0)
+		arena->ctime = arena->wtime;
+	qunlock(&arena->lock);
+	return aa;
+}
+
+/*
+ * once sealed, an arena never has any data added to it.
+ * it should only be changed to fix errors.
+ * this also syncs the clump directory.
+ */
+static void
+sealarena(Arena *arena)
+{
+	arena->inqueue = 1;
+	backsumarena(arena);
+}
+
+void
+backsumarena(Arena *arena)
+{
+	ASum *as;
+
+	as = MK(ASum);
+	if(as == nil)
+		return;
+	qlock(&sumlock);
+	as->arena = arena;
+	as->next = nil;
+	if(sumq)
+		sumqtail->next = as;
+	else
+		sumq = as;
+	sumqtail = as;
+	/*
+	 * Might get here while initializing arenas,
+	 * before initarenasum has been called.
+	 */
+	if(sumwait.l)
+		rwakeup(&sumwait);
+	qunlock(&sumlock);
+}
+
+static void
+sumproc(void *unused)
+{
+	ASum *as;
+	Arena *arena;
+
+	USED(unused);
+
+	for(;;){
+		qlock(&sumlock);
+		while(sumq == nil)
+			rsleep(&sumwait);
+		as = sumq;
+		sumq = as->next;
+		qunlock(&sumlock);
+		arena = as->arena;
+		free(as);
+		sumarena(arena);
+	}
+}
+
+void
+sumarena(Arena *arena)
+{
+	ZBlock *b;
+	DigestState s;
+	u64int a, e;
+	u32int bs;
+	int t;
+	u8int score[VtScoreSize];
+
+	bs = MaxIoSize;
+	if(bs < arena->blocksize)
+		bs = arena->blocksize;
+
+	/*
+	 * read & sum all blocks except the last one
+	 */
+	memset(&s, 0, sizeof s);
+	b = alloczblock(bs, 0, arena->part->blocksize);
+	e = arena->base + arena->size;
+	for(a = arena->base - arena->blocksize; a + arena->blocksize <= e; a += bs){
+		while((t=arenasumsleeptime) == SleepForever){
+			sleep(1000);
+		}
+		sleep(t);
+		if(a + bs > e)
+			bs = arena->blocksize;
+		if(readpart(arena->part, a, b->data, bs) < 0)
+			goto ReadErr;
+		addstat(StatSumRead, 1);
+		addstat(StatSumReadBytes, bs);
+		sha1(b->data, bs, nil, &s);
+	}
+
+	/*
+	 * the last one is special, since it may already have the checksum included
+	 */
+	bs = arena->blocksize;
+	if(readpart(arena->part, e, b->data, bs) < 0){
+ReadErr:
+		logerr(EOk, "sumarena can't sum %s, read at %lld failed: %r", arena->name, a);
+		freezblock(b);
+		return;
+	}
+	addstat(StatSumRead, 1);
+	addstat(StatSumReadBytes, bs);
+
+	sha1(b->data, bs-VtScoreSize, nil, &s);
+	sha1(zeroscore, VtScoreSize, nil, &s);
+	sha1(nil, 0, score, &s);
+
+	/*
+	 * check for no checksum or the same
+	 */
+	if(scorecmp(score, &b->data[bs - VtScoreSize]) != 0
+	&& scorecmp(zeroscore, &b->data[bs - VtScoreSize]) != 0)
+		logerr(EOk, "overwriting mismatched checksums for arena=%s, found=%V calculated=%V",
+			arena->name, &b->data[bs - VtScoreSize], score);
+	freezblock(b);
+
+	qlock(&arena->lock);
+	scorecp(arena->score, score);
+	wbarena(arena);
+	qunlock(&arena->lock);
+}
+
+/*
+ * write the arena trailer block to the partition
+ */
+int
+wbarena(Arena *arena)
+{
+	DBlock *b;
+	int bad;
+
+	if((b = getdblock(arena->part, arena->base + arena->size, OWRITE)) == nil){
+		logerr(EAdmin, "can't write arena trailer: %r");
+		return -1;
+	}
+	dirtydblock(b, DirtyArenaTrailer);
+	bad = okarena(arena)<0 || packarena(arena, b->data)<0;
+	scorecp(b->data + arena->blocksize - VtScoreSize, arena->score);
+	putdblock(b);
+	if(bad)
+		return -1;
+	return 0;
+}
+
+int
+wbarenahead(Arena *arena)
+{
+	ZBlock *b;
+	ArenaHead head;
+	int bad;
+
+	namecp(head.name, arena->name);
+	head.version = arena->version;
+	head.size = arena->size + 2 * arena->blocksize;
+	head.blocksize = arena->blocksize;
+	head.clumpmagic = arena->clumpmagic;
+	b = alloczblock(arena->blocksize, 1, arena->part->blocksize);
+	if(b == nil){
+		logerr(EAdmin, "can't write arena header: %r");
+/* ZZZ add error message? */
+		return -1;
+	}
+	/*
+	 * this writepart is okay because it only happens
+	 * during initialization.
+	 */
+	bad = packarenahead(&head, b->data)<0 ||
+	      writepart(arena->part, arena->base - arena->blocksize, b->data, arena->blocksize)<0 ||
+	      flushpart(arena->part)<0;
+	freezblock(b);
+	if(bad)
+		return -1;
+	return 0;
+}
+
+/*
+ * read the arena header and trailer blocks from disk
+ */
+static int
+loadarena(Arena *arena)
+{
+	ArenaHead head;
+	ZBlock *b;
+
+	b = alloczblock(arena->blocksize, 0, arena->part->blocksize);
+	if(b == nil)
+		return -1;
+	if(readpart(arena->part, arena->base + arena->size, b->data, arena->blocksize) < 0){
+		freezblock(b);
+		return -1;
+	}
+	if(unpackarena(arena, b->data) < 0){
+		freezblock(b);
+		return -1;
+	}
+	if(arena->version != ArenaVersion4 && arena->version != ArenaVersion5){
+		seterr(EAdmin, "unknown arena version %d", arena->version);
+		freezblock(b);
+		return -1;
+	}
+	scorecp(arena->score, &b->data[arena->blocksize - VtScoreSize]);
+
+	if(readpart(arena->part, arena->base - arena->blocksize, b->data, arena->blocksize) < 0){
+		logerr(EAdmin, "can't read arena header: %r");
+		freezblock(b);
+		return 0;
+	}
+	if(unpackarenahead(&head, b->data) < 0)
+		logerr(ECorrupt, "corrupted arena header: %r");
+	else if(namecmp(arena->name, head.name)!=0
+	     || arena->clumpmagic != head.clumpmagic
+	     || arena->version != head.version
+	     || arena->blocksize != head.blocksize
+	     || arena->size + 2 * arena->blocksize != head.size){
+		if(namecmp(arena->name, head.name)!=0)
+			logerr(ECorrupt, "arena tail name %s head %s",
+				arena->name, head.name);
+		else if(arena->clumpmagic != head.clumpmagic)
+			logerr(ECorrupt, "arena tail clumpmagic 0x%lux head 0x%lux",
+				(ulong)arena->clumpmagic, (ulong)head.clumpmagic);
+		else if(arena->version != head.version)
+			logerr(ECorrupt, "arena tail version %d head version %d",
+				arena->version, head.version);
+		else if(arena->blocksize != head.blocksize)
+			logerr(ECorrupt, "arena tail block size %d head %d",
+				arena->blocksize, head.blocksize);
+		else if(arena->size+2*arena->blocksize != head.size)
+			logerr(ECorrupt, "arena tail size %lud head %lud",
+				(ulong)arena->size+2*arena->blocksize, head.size);
+		else
+			logerr(ECorrupt, "arena header inconsistent with arena data");
+	}
+	freezblock(b);
+
+	return 0;
+}
+
+static int
+okarena(Arena *arena)
+{
+	u64int dsize;
+	int ok;
+
+	ok = 0;
+	dsize = arenadirsize(arena, arena->diskstats.clumps);
+	if(arena->diskstats.used + dsize > arena->size){
+		seterr(ECorrupt, "arena %s used > size", arena->name);
+		ok = -1;
+	}
+
+	if(arena->diskstats.cclumps > arena->diskstats.clumps)
+		logerr(ECorrupt, "arena %s has more compressed clumps than total clumps", arena->name);
+
+	/*
+	 * This need not be true if some of the disk is corrupted.
+	 *
+	if(arena->diskstats.uncsize + arena->diskstats.clumps * ClumpSize + arena->blocksize < arena->diskstats.used)
+		logerr(ECorrupt, "arena %s uncompressed size inconsistent with used space %lld %d %lld", arena->name, arena->diskstats.uncsize, arena->diskstats.clumps, arena->diskstats.used);
+	 */
+
+	/*
+	 * this happens; it's harmless.
+	 *
+	if(arena->ctime > arena->wtime)
+		logerr(ECorrupt, "arena %s creation time after last write time", arena->name);
+	 */
+	return ok;
+}
+
+static CIBlock*
+getcib(Arena *arena, int clump, int writing, CIBlock *rock)
+{
+	int mode;
+	CIBlock *cib;
+	u32int block, off;
+
+	if(clump >= arena->memstats.clumps){
+		seterr(EOk, "clump directory access out of range");
+		return nil;
+	}
+	block = clump / arena->clumpmax;
+	off = (clump - block * arena->clumpmax) * ClumpInfoSize;
+	cib = rock;
+	cib->block = block;
+	cib->offset = off;
+
+	if(writing){
+		if(off == 0 && clump == arena->memstats.clumps-1)
+			mode = OWRITE;
+		else
+			mode = ORDWR;
+	}else
+		mode = OREAD;
+
+	cib->data = getdblock(arena->part,
+		arena->base + arena->size - (block + 1) * arena->blocksize, mode);
+	if(cib->data == nil)
+		return nil;
+	return cib;
+}
+
+static void
+putcib(Arena *arena, CIBlock *cib)
+{
+	USED(arena);
+
+	putdblock(cib->data);
+	cib->data = nil;
+}
+
+/* these are from dcache.c */
+DBlock staticdblock;
+DBlock *getdblock(Part *part, u64int addr, int mode){
+	USED(mode);
+//	return part->mapped+addr;
+	staticdblock.data= part->mapped+addr;
+	return &staticdblock;
+}
+void initdcache(u32int n) {}
+void kickdcache(void) {}
+void emptydcache(void) {}
+void flushdcache(void) {}
+void dirtydblock(DBlock *d,int n) {}
+void putdblock(DBlock *b) {}
+
---
+++ src/cmd/venti/mappedsrv/part.c
@@ -0,0 +1,356 @@
+#include "stdinc.h"
+#include <ctype.h>
+#include "dat.h"
+#include "fns.h"
+#include <sys/mman.h>
+
+u32int	maxblocksize;
+#pragma weak readonly
+int	readonly;
+
+int findsubpart(Part *part, char *name);
+
+static int
+strtoullsuf(char *p, char **pp, int rad, u64int *u)
+{
+	u64int v;
+
+	if(!isdigit((uchar)*p))
+		return -1;
+	v = strtoull(p, &p, rad);
+	switch(*p){
+	case 'k':
+	case 'K':
+		v *= 1024;
+		p++;
+		break;
+	case 'm':
+	case 'M':
+		v *= 1024*1024;
+		p++;
+		break;
+	case 'g':
+	case 'G':
+		v *= 1024*1024*1024;
+		p++;
+		break;
+	case 't':
+	case 'T':
+		v *= 1024*1024;
+		v *= 1024*1024;
+		p++;
+		break;
+	}
+	*pp = p;
+	*u = v;
+	return 0;
+}
+
+static int
+parsepart(char *name, char **file, char **subpart, u64int *lo, u64int *hi)
+{
+	char *p;
+
+	*file = estrdup(name);
+	*lo = 0;
+	*hi = 0;
+	*subpart = nil;
+	if((p = strrchr(*file, ':')) == nil)
+		return 0;
+	*p++ = 0;
+	if(isalpha(*p)){
+		*subpart = p;
+		return 0;
+	}
+	if(*p == '-')
+		*lo = 0;
+	else{
+		if(strtoullsuf(p, &p, 0, lo) < 0){
+			free(*file);
+			return -1;
+		}
+	}
+	if(*p == '-')
+		p++;
+	if(*p == 0){
+		*hi = 0;
+		return 0;
+	}
+	if(strtoullsuf(p, &p, 0, hi) < 0 || *p != 0){
+		free(*file);
+		return -1;
+	}
+	return 0;
+}
+
+#undef min
+#define min(a, b) ((a) < (b) ? (a) : (b))
+Part*
+initpart(char *name, int mode)
+{
+	Part *part;
+	Dir *dir;
+	char *file, *subname;
+	u64int lo, hi;
+
+	if(parsepart(name, &file, &subname, &lo, &hi) < 0){
+		werrstr("cannot parse name %s", name);
+		return nil;
+	}
+	trace(TraceDisk, "initpart %s file %s lo 0x%llx hi 0x%llx", name, file, lo, hi);
+	part = MKZ(Part);
+	part->name = estrdup(name);
+	part->filename = estrdup(file);
+	if(readonly){
+		mode &= ~(OREAD|OWRITE|ORDWR);
+		mode |= OREAD;
+	}
+	part->fd = open(file, mode);
+	if(part->fd < 0){
+		if((mode&(OREAD|OWRITE|ORDWR)) == ORDWR)
+			part->fd = open(file, (mode&~ORDWR)|OREAD);
+		if(part->fd < 0){
+			freepart(part);
+			fprint(2, "can't open partition='%s': %r\n", file);
+			seterr(EOk, "can't open partition='%s': %r", file);
+			fprint(2, "%r\n");
+			free(file);
+			return nil;
+		}
+		fprint(2, "warning: %s opened for reading only\n", name);
+	}
+	part->offset = lo;
+	dir = dirfstat(part->fd);
+	if(dir == nil){
+		freepart(part);
+		seterr(EOk, "can't stat partition='%s': %r", file);
+		free(file);
+		return nil;
+	}
+	if(dir->length == 0){
+		free(dir);
+		dir = dirstat(file);
+		if(dir == nil || dir->length == 0) {
+			freepart(part);
+			seterr(EOk, "can't determine size of partition %s", file);
+			free(file);
+			return nil;
+		}
+	}
+	if(dir->length < hi || dir->length < lo){
+		freepart(part);
+		seterr(EOk, "partition '%s': bounds out of range (max %lld)", name, dir->length);
+		free(dir);
+		free(file);
+		return nil;
+	}
+	if(hi == 0)
+		hi = dir->length;
+	part->size = hi - part->offset;
+#ifdef CANBLOCKSIZE
+	{
+		struct statfs sfs;
+		if(fstatfs(part->fd, &sfs) >= 0 && sfs.f_bsize > 512)
+			part->fsblocksize = sfs.f_bsize;
+	}
+#endif
+
+	part->fsblocksize = min(part->fsblocksize, MaxIo);
+
+	if(subname && findsubpart(part, subname) < 0){
+		werrstr("cannot find subpartition %s", subname);
+		freepart(part);
+		return nil;
+	}
+	free(dir);
+	part->mapped = mmap(NULL, part->size, PROT_READ, MAP_SHARED, part->fd, part->offset);
+	fprint(2, "%s map: %llx, size: %lld, offset: %lld\n", part->name, part->mapped,part->size,part->offset);
+	return part;
+}
+
+int
+flushpart(Part *part)
+{
+	USED(part);
+	return 0;
+}
+
+void
+freepart(Part *part)
+{
+	if(part == nil)
+		return;
+	if(part->mapped) { munmap( part->mapped, part->size); part->mapped=0; }
+	if(part->fd >= 0)
+		close(part->fd);
+	free(part->name);
+	free(part);
+}
+
+void
+partblocksize(Part *part, u32int blocksize)
+{
+	if(part->blocksize)
+		sysfatal("resetting partition=%s's block size", part->name);
+	part->blocksize = blocksize;
+	if(blocksize > maxblocksize)
+		maxblocksize = blocksize;
+}
+
+#ifndef PLAN9PORT
+static int sdreset(Part*);
+static int threadspawnl(int[3], char*, char*, ...);
+#endif
+
+static int
+rwpart(Part *part, int isread, u64int offset, u8int *buf, u32int count)
+{
+	trace(TraceDisk, "%s %s %ud at 0x%llx",
+		isread ? "read" : "write", part->name, count, offset);
+	if(offset >= part->size || offset+count > part->size){
+		seterr(EStrange, "out of bounds %s offset 0x%llux count %ud to partition %s size 0x%llux",
+			isread ? "read" : "write", offset, count, part->name, part->size);
+		return -1;
+	}
+	if(isread)
+		memmove(buf, part->mapped+offset, count);
+	else{
+		memmove(part->mapped+offset, buf, count);
+	}
+	return count;
+}
+int
+readpart(Part *part, u64int offset, u8int *buf, u32int count)
+{
+	return rwpart(part, 1, offset, buf, count);
+}
+
+int
+writepart(Part *part, u64int offset, u8int *buf, u32int count)
+{
+	return rwpart(part, 0, offset, buf, count);
+}
+
+ZBlock*
+readfile(char *name)
+{
+	Part *p;
+	ZBlock *b;
+
+	p = initpart(name, OREAD);
+	if(p == nil)
+		return nil;
+	b = alloczblock(p->size, 0, p->blocksize);
+	if(b == nil){
+		seterr(EOk, "can't alloc %s: %r", name);
+		freepart(p);
+		return nil;
+	}
+	if(readpart(p, 0, b->data, p->size) < 0){
+		seterr(EOk, "can't read %s: %r", name);
+		freepart(p);
+		freezblock(b);
+		return nil;
+	}
+	freepart(p);
+	return b;
+}
+
+/*
+ * Search for the Plan 9 partition with the given name.
+ * This lets you write things like /dev/ad4:arenas
+ * if you move a disk from a Plan 9 system to a FreeBSD system.
+ *
+ * God I hope I never write this code again.
+ */
+#define MAGIC "plan9 partitions"
+static int
+tryplan9part(Part *part, char *name)
+{
+	uchar buf[512];
+	char *line[40], *f[4];
+	int i, n;
+	vlong start, end;
+
+	/*
+	 * Partition table in second sector.
+	 * Could also look on 2nd last sector and last sector,
+	 * but those disks died out long before venti came along.
+	 */
+	if(readpart(part, 512, buf, 512) != 512)
+		return -1;
+
+	/* Plan 9 partition table is just text strings */
+	if(strncmp((char*)buf, "part ", 5) != 0)
+		return -1;
+
+	buf[511] = 0;
+	n = getfields((char*)buf, line, 40, 1, "\n");
+	for(i=0; i<n; i++){
+		if(getfields(line[i], f, 4, 1, " ") != 4)
+			break;
+		if(strcmp(f[0], "part") != 0)
+			break;
+		if(strcmp(f[1], name) == 0){
+			start = 512*strtoll(f[2], 0, 0);
+			end = 512*strtoll(f[3], 0, 0);
+			if(start  < end && end <= part->size){
+				part->offset += start;
+				part->size = end - start;
+				return 0;
+			}
+			return -1;
+		}
+	}
+	return -1;
+}
+
+#define	GSHORT(p)	(((p)[1]<<8)|(p)[0])
+#define	GLONG(p)	((GSHORT(p+2)<<16)|GSHORT(p))
+
+typedef struct Dospart Dospart;
+struct Dospart
+{
+	uchar flag;		/* active flag */
+	uchar shead;		/* starting head */
+	uchar scs[2];		/* starting cylinder/sector */
+	uchar type;		/* partition type */
+	uchar ehead;		/* ending head */
+	uchar ecs[2];		/* ending cylinder/sector */
+	uchar offset[4];		/* starting sector */
+	uchar size[4];		/* length in sectors */
+};
+
+
+int
+findsubpart(Part *part, char *name)
+{
+	int i;
+	uchar buf[512];
+	u64int size;
+	Dospart *dp;
+
+	/* See if this is a Plan 9 partition. */
+	if(tryplan9part(part, name) >= 0)
+		return 0;
+
+	/* Otherwise try for an MBR and then narrow to Plan 9 partition. */
+	if(readpart(part, 0, buf, 512) != 512)
+		return -1;
+	if(buf[0x1FE] != 0x55 || buf[0x1FF] != 0xAA)
+		return -1;
+	dp = (Dospart*)(buf+0x1BE);
+	size = part->size;
+	for(i=0; i<4; i++){
+		if(dp[i].type == '9'){
+			part->offset = 512LL*GLONG(dp[i].offset);
+			part->size = 512LL*GLONG(dp[i].size);
+			if(tryplan9part(part, name) >= 0)
+				return 0;
+			part->offset = 0;
+			part->size = size;
+		}
+		/* Not implementing extended partitions - enough is enough. */
+	}
+	return -1;
+}
